ba5b251744c9f9057257af10160dec3e4834b421
Joseph Catrambone <me@josephcatrambone.com>
2026-04-10 07:41:45 -0700

    Placeholder for game state management.

---
+++ main/gamestates/gamestate.gd
@@ -0,0 +1,14 @@
+
+#@abstract 
+class_name GameState
+extends Resource
+
+enum State {
+	TITLE, PAUSE_MENU, PLAYING_LEVEL, CUTSCENE, ENDING
+}
+
+
+"""
+@abstract func on_push()
+@abstract func on_pop()
+"""
